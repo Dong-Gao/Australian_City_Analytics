5a8ab9b57494d5990ec3e484394913196ffc9970
nanjiangli92 <lnj13579@126.com>
2017-04-20 14:35:02 +1000

    Add files via upload

---
+++ smoke-drink.py
@@ -0,0 +1,29 @@
+import re
+
+smokeResult={'negative':0, 'positive':0}
+def get_train():
+    train=[]
+    file=open('smoke.txt', 'r', encoding='utf-8')
+    while 1:
+        line=file.readline().strip('\n')
+        train.append(line)
+        if not line:
+            break
+    return  train
+def score2dic(score):
+    if score>=0:
+        smokeResult['positive']= smokeResult['positive'] + 1
+    else:
+        smokeResult['negative']= smokeResult['negative'] + 1
+def smokeAndDrink(input):
+    find = 0
+    text=input['text']
+    trains=get_train()
+    for t in trains:
+        if t in text:
+            find=find+1
+        else:
+            find=find
+    if find>0:
+        score=senti_analy(input)
+        score2dic(score)
