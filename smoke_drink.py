9c8a802ef6344daafb9214b416234559c0c8dbd2
nanjiangli92 <lnj13579@126.com>
2017-04-23 14:48:03 +1000

    Update smoke_drink.py

--- smoke_drink.py
+++ smoke_drink.py
@@ -4,26 +4,26 @@ import couchdb
 
 
 
-def get_train():
-    train=[]
+def get_words():
+    words=[]
     file=open('smoke.txt', 'r', encoding='utf-8')
     while 1:
         line=file.readline().strip('\n')
-        train.append(line)
+        words.append(line)
         if not line:
             break
-    return  train
+    return  words
 def score2dic(score):
     if score>=0:
         smokeResult['positive']= smokeResult['positive'] + 1
     else:
         smokeResult['negative']= smokeResult['negative'] + 1
-def find(tweet,smokeResult):
-    tains=get_train()
+def find(tweet,words,smokeResult):
+    
     text=tweet['text']
     loc=tweet['location']
     find = 0
-    for t in trains:
+    for t in words:
         if t in text:
             find=find+1
         else:
@@ -39,9 +39,10 @@ def find(tweet,smokeResult):
                     smokeResult[l]['negative']=smokeResult[l]['negative']+1
     return smokeResult
 def smoke_Drink(smokeResult):
+    words=get_words()
     server = couchdb.Server('placeholer')
     db=server['placeholer']
     for tweet in db:
-        find(tweet,smokeResult)
+        find(tweet,words,smokeResult)
     return smokeResult
     
